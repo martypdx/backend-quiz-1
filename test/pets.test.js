63f972a18446181ee8580b147f5b31748dada8cd
martypdx <marty@martypdx.com>
2017-11-06 09:52:51 -0800

    initial test so travis ci passes at start

--- test/pets.test.js
+++ test/pets.test.js
@@ -6,5 +6,9 @@ describe('<Resource Name Here> API', () => {
     
     beforeEach(() => mongoose.connection.dropDatabase());
 
+    // remove me!
+    it('is a test', () => {
+        assert.isOk(request);
+    });
 
 });
